955457873c60464bc4485afd90ee12fd3c7a87df
Eduardo Sanchez <80008158+lorf543@users.noreply.github.com>
2024-08-03 10:48:15 -0400

    static2

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',  
     'django_ckeditor_5',
     'blog',
